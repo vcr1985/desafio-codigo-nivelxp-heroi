819b5db95a7ad50e39158a45e3580affd6b2f97d
Vando Ramos <ramosvando@gmail.com>
2023-10-04 18:13:14 -0300

    projeto criado

---
+++ index.js
@@ -0,0 +1,27 @@
+//Variaveis para armazenar o nome XP do herói
+const nome = "Hulk";
+const xp =7000;
+//variavel para armazenar o nivel
+let nivel;
+ 
+//Estrutura de decisão para determinar o nivek XP`
+
+if (xp < 1000) {
+    nivel = "Ferro"
+}else if (xp >= 1001 && xp <= 2000) {
+    nivel = "Bronze";
+}else if (xp >=2001 &&  xp <= 5000){
+    nivel = "Prata";
+}else if (xp >= 6001 && xp <= 7000) {
+    nivel = "Ouro";
+}else if (xp >= 7001 && xp <= 8000) {
+    nivel = "Platina Diamante";
+}else if (xp >= 8001 && xp <+ 9000) {
+    nivel = "Ascendente";
+}else if (xp >= 9001 && xp <= 10000) {
+    nivel = "Imortal";
+}else {
+    nivel = "Radiante";
+}
+
+console.log(`O Herói de nome ${nome} está no  nivel de ${nivel}.`);
